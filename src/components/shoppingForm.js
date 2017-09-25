8deca5c24f445a63bcb97704848cf5aa7c23d3a7
Fanny Vieira <fannyvieiira@hotmail.com>
2017-09-25 14:49:15 -0300

    add pure components

--- src/components/shoppingForm.js
+++ src/components/shoppingForm.js
@@ -1,18 +1,17 @@
-import React, {Component} from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 
-class ShoppingForm extends Component{
-    render(){
-        return(
-            <div className="header">
-                <form onSubmit={this.props.handleSubmit}>
-                    <input type="text" name="product" placeholder="Product" onChange={this.props.handleChange}/><br/><br/>
-                    <input type="number" name="quantity" placeholder="Quantity" onChange={this.props.handleChange}/><br/><br/>
-                    <button type="submit">Add to Shopping List</button><br/><br/>
-                </form>
-            </div>
-        );
-    }
+const ShoppingForm = (props) => {
+    const { handleChange, handleSubmit } = props;
+    return(
+        <div className="header">
+            <form onSubmit={handleSubmit}>
+                <input type="text" name="product" placeholder="Product" onChange={handleChange}/><br/><br/>
+                <input type="number" name="quantity" placeholder="Quantity" onChange={handleChange}/><br/><br/>
+                <button type="submit">Add to Shopping List</button><br/><br/>
+            </form>
+        </div>
+    );
 };
 
 ShoppingForm.PropTypes = {
